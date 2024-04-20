9e57c66a42ac675c41a66782dfdb8e2486cb69bb
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-04-20 15:44:46 +0200

    new

--- hangman.c
+++ hangman.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 #include "hangman.h"
 
 
@@ -31,4 +32,38 @@ int get_word(char secret[]){
     fclose(fp);
 
     return 0;
+}
+int is_word_guessed(const char secret[], const char letters_guessed[]) {
+    int findFlag = 0;
+    for (int i = 0; i < strlen(secret); i++) {
+        findFlag = 0;
+        for (int j = 0; j < strlen(letters_guessed); j++) {
+            if (secret[i] == letters_guessed[j]) {
+                findFlag = 1;
+                break;
+            }
+        }
+        if (findFlag == 0) {
+            return 0;
+        }   
+    }
+    return 1;
+}
+
+void get_guessed_word(const char secret[], const char letters_guessed[], char guessed_word[]) {
+    for (int i = 0; i < strlen(secret); i++) {
+        int letter_guessed = 0;
+        for (int j = 0; j < strlen(letters_guessed); j++) {
+            if (secret[i] == letters_guessed[j]) {
+                letter_guessed = 1;
+                break;
+            }
+        }
+        if (letter_guessed) {
+            guessed_word[i] = secret[i];
+        } else {
+            guessed_word[i] = '_';
+        }
+    }
+    guessed_word[strlen(secret)] = '\0';
 }
---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "hangman.h"
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+    srand(time(NULL));
+    char secret[16];
+    get_word(secret);
+    printf("%s\n", secret);
+
+    printf("%d\n", is_word_guessed("secret","aeiou"));
+    // prints: 0
+    printf("%d\n", is_word_guessed("hello","aeihoul"));
+    // prints^ 1
+    char resalt[30];
+    get_guessed_word("container", "arpstxgoieyu", resalt);
+    printf("%s\n", resalt);  // Выведет: "_o_tai_er"
+}
